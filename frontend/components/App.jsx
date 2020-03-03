89535a7de2c7c4428c0fafd0a6d7b603765ce389
HyoL <hyolim.je9@gmail.com>
2020-03-03 14:55:08 -0800

    Adding user auth backend

---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import GreetingContainer from "./greeting_container";
+// import SignupFormContainer from "./signup_form_container";
+
+
+const App = () => (
+    <div>
+        <h1>Yillow</h1>
+        {/* <GreetingContainer /> */}
+        {/* <SignupFormContainer /> */}
+    </div>
+);
+
+export default App;
---
+++ frontend/yillow.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from './store/store';
+import Root from "./components/root";
+
+// import * as SessionApiUtil from './util/session_api_util';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store} />, root);
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    
+    // window.signup = SessionApiUtil.signup;
+    // window.login = SessionApiUtil.login;
+    // window.logout = SessionApiUtil.logout;
+});
