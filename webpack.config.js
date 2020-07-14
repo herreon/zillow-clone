1db91de6053e4bfb8b4a6662aab9efd267d75906
HyoL <58346289+herreon@users.noreply.github.com>
2020-07-14 11:43:26 -0700

    Renaming

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./frontend/yillow.jsx",
+    entry: "./frontend/zillowne.jsx",
     output: {
         path: path.resolve(__dirname, "app", "assets", "javascripts"),
         filename: "./bundle.js"
